08d4f428c826e334f7e5374224ffe36c012e66c4
toto <antonios.gerges4@gmail.com>
2026-05-17 12:03:56 +0300

    feat: "using useLayoutEffect for better ui, removing Text that i am not using,"

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,13 +1,22 @@
-import {MEALS} from "../data/dummy-data";
-import {View, Text, StyleSheet, FlatList} from "react-native";
+import {useLayoutEffect} from "react";
+import {View, StyleSheet, FlatList} from "react-native";
+import {MEALS, CATEGORIES} from "../data/dummy-data";
 import MealItem from "../components/MealItem";
 
-function MealsOverviewScreen({ route }) {
+function MealsOverviewScreen({ route, navigation   }) {
    const catId = route.params.categoryId;
 
    const displayedMeals = MEALS.filter((mealItem) => {
        return mealItem.categoryIds.indexOf(catId) >=0;
    });
+
+   useLayoutEffect(() => {
+       const categoryTitle = CATEGORIES.find((category) => category.id === catId).title ;
+
+       navigation.setOptions({
+           title: categoryTitle,
+       });
+   },[catId, navigation]);
 
    function renderMealItem(itemData) {
        const item = itemData.item;
